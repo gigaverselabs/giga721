1ad2c00957611c05904c56c0d93066853b69e308
stopak <1740874+stopak@users.noreply.github.com>
2022-04-04 22:47:20 +0200

    Added variables to deploy script

---
+++ variables.example.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+NAME="\"Project Name\""
+TICKER="\"Project Ticker\""
+DESCRIPTION="\"Project Description\""
+SIZE=10000 # Number of tokens
+
+# OWNER=principal # Used to set owner of the token canister
