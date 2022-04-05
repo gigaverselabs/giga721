ed79b5448f9eb78fb89e2af44768d19ef470a598
stopak <1740874+stopak@users.noreply.github.com>
2022-04-05 11:59:49 +0200

    Updated network deploy_scripts

--- network_upgrade.sh
+++ network_upgrade.sh
@@ -4,13 +4,8 @@ PUBLIC_KEY="principal \"$( \
     dfx identity get-principal
 )\""
 
-dfx canister --network ic create token
-# dfx canister --network ic create ledger_proxy
-
 dfx build --network ic token
-# dfx build --network ic ledger_proxy
 
-eval dfx canister --network ic install token --argument="'(\"Infinity Flies\", \"IFNFT\", \"\", 10000, $PUBLIC_KEY)'" -m upgrade
-# eval dfx canister --network ic install ledger_proxy -m upgrade
+eval dfx canister --network ic install token --argument="'($NAME, $TICKER, $DESCRIPTION, $SIZE, $PUBLIC_KEY)'" -m upgrade
 
 echo "Upgrade complete"
