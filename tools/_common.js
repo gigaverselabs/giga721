a4521d665a153c475ef16f1fb85884c14535080a
Adam Stępnik <adam@excodus.com>
2022-04-05 22:23:26 +0200

    Expanded giga api

--- tools/_common.js
+++ tools/_common.js
@@ -2,6 +2,7 @@ import { Actor, HttpAgent } from '@dfinity/agent';
 import { Ed25519KeyIdentity } from '@dfinity/identity';
 
 import { idlFactory } from '../.dfx/ic/canisters/token/token.did.js';
+import { idlFactory as proxy_idlFactory } from '../.dfx/ic/canisters/ledger_proxy/ledger_proxy.did.js';
 import fetch from 'node-fetch';
 import fs from 'fs';
 
@@ -27,6 +28,22 @@ function getCanisterId(useProd) {
     return canisterId;
 }
 
+function getProxyId(useProd) {
+    let canisterId = null;
+
+    if (useProd) {
+        var data = JSON.parse(fs.readFileSync("../canister_ids.json"))
+        canisterId = data["ledger_proxy"]["ic"];
+
+    } else {
+        var data = JSON.parse(fs.readFileSync("../.dfx/local/canister_ids.json"))
+        canisterId = data["ledger_proxy"]["local"];
+    }
+
+    console.log("Proxy Canister Id: " + canisterId);
+    return canisterId;
+}
+
 //Returns actor for token canister
 export function getActor(useProd) {
 
@@ -45,6 +62,31 @@ export function getActor(useProd) {
     }
 
     const actor = Actor.createActor(idlFactory, {
+        agent: httpAgent,
+        canisterId: canisterId,
+    });
+
+    return actor;
+}
+
+//Returns actor for token canister
+export function getLedgerActor(useProd) {
+
+    let httpAgent = null;
+    let canisterId = getProxyId(useProd);
+
+    if (useProd) {
+        var host = "https://boundary.ic0.app/"; //ic
+
+        httpAgent = new HttpAgent({ identity: key, host });
+    } else {
+        const host = "http://127.0.0.1:8000"; //local
+
+        httpAgent = new HttpAgent({ identity: key, host });
+        httpAgent.fetchRootKey();
+    }
+
+    const actor = Actor.createActor(proxy_idlFactory, {
         agent: httpAgent,
         canisterId: canisterId,
     });
--- tools/_data_converter.example.js
+++ tools/_data_converter.example.js
@@ -1,27 +1,41 @@
 export function convert_data(data) {
-    const props = [
-      'Background',
-      'Head',
-      'Expression',
-      'Mouth',
-      'Top',
-      'Face',
-    ];
-  
-    let item = {
-      id: data.tokenId,
-      url: '/Token/'+data.tokenId,
-      name: 'Wojak #' + data.tokenId,
-      desc: '',
-      properties: []
-    }
-  
-    for (let i = 0; i < props.length; i++) {
-      let name = props[i];
-      let value = data[name];
-  
-      item.properties.push({ 'name': name, 'value': value });
-    }
-  
-    return item;
+  const props = [
+    'Background',
+    'Head',
+    'Expression',
+    'Mouth',
+    'Top',
+    'Face',
+  ];
+
+  let item = {
+    id: data.tokenId,
+    url: '/Token/' + data.tokenId,
+    name: 'Wojak #' + data.tokenId,
+    desc: '',
+    properties: []
   }
+
+  for (let i = 0; i < props.length; i++) {
+    let name = props[i];
+    let value = data[name];
+
+    item.properties.push({ 'name': name, 'value': value });
+  }
+
+  return item;
+}
+
+import fs from 'fs';
+
+export function getMetadata() {
+  let metadata = JSON.parse(fs.readFileSync('../../json/_metadata.json'));
+
+  let data = [];
+
+  for (var i = 0; i < metadata.length; i++) {
+    let item = convert_data(metadata[i]);
+
+    data.push(item);
+  }
+}
