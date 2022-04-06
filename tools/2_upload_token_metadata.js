6517e51939b7f471df226ea855f9420f09614908
stopak <1740874+stopak@users.noreply.github.com>
2022-04-06 21:07:29 +0200

    Made tool 2 more generic

--- tools/2_upload_token_metadata.js
+++ tools/2_upload_token_metadata.js
@@ -1,19 +1,13 @@
 import { getActor } from "./_common.js";
-import { convert_data } from "./_data_converter.js";
-import fs from 'fs';
+import { getMetadata } from "./_data_converter.js";
 
 //Mints new tokens using multi_mint feature, before sending package of tokens to mint, checks if the request is within max_size limits (currently 2mb of data)
 async function run() {
   let actor = getActor(true);
-  let metadata = JSON.parse(fs.readFileSync('../../json/_metadata.json'));
 
-  let data = [];
 
-  for (var i = 0; i < metadata.length; i++) {
-    let item = convert_data(metadata[i]);
+  let data = getMetadata();
 
-    data.push(item);
-  }
   try {
     let result = await actor.upload_tokens_metadata(data);
 
