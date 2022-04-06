c9e0207fa7a18a7e13fec67ced798bba95c35712
stopak <1740874+stopak@users.noreply.github.com>
2022-04-06 21:03:22 +0200

    added proxy scripts

--- network_reinstall_proxy.sh
+++ network_reinstall_proxy.sh
@@ -10,7 +10,7 @@ dfx canister --network ic create ledger_proxy
 
 dfx build --network ic ledger_proxy
 
-eval dfx canister --network ic install ledger_proxy
+eval dfx canister --network ic install ledger_proxy -m reinstall
 
 echo "Installation complete"
 
@@ -20,7 +20,7 @@ TOKENID="principal \"$TOKENID\""
 LEDGERID=$(dfx canister --network ic id ledger_proxy)
 LEDGERID="principal \"$LEDGERID\""
 
-eval dfx canister --network ic call token set_ledger_canister "'($LEDGERID)'"
+# eval dfx canister --network ic call token set_ledger_canister "'($LEDGERID)'"
 eval dfx canister --network ic call ledger_proxy set_token_canister "'($TOKENID)'"
 
 echo "Preparation complete"
